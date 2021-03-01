a0ed89ad6a1ed612cc762c3886963d7bfc06c346
d040110 <eduardo@populate.tools>
2021-03-01 21:54:22 +0100

    Adapt import processes services to localized data and images

--- app/services/processes_parser.rb
+++ app/services/processes_parser.rb
@@ -2,7 +2,7 @@
 
 # An example implementation of an AuthorizationHandler to be used in tests.
 class ProcessesParser
-  attr_reader :raw_content, :organization
+  attr_reader :raw_content, :organization, :locale
 
   MONTH_NAMES = {
     "Enero" => 1,
@@ -19,25 +19,37 @@ class ProcessesParser
     "Diciembre" => 12
   }.freeze
 
+  TRANSLATIONS = {
+    es: {
+      documentation: "Documentación",
+      links: "Enlaces"
+    },
+    eu: {
+      documentation: "Dokumentazioa",
+      links: "Estekak"
+    }
+  }.with_indifferent_access.freeze
+
   def initialize(row, organization)
     @raw_content = row
     @organization = organization
+    @locale = raw_content["Idioma"] == "Euskera" ? "eu" : "es"
   end
 
   def transformed_data
     {
-      "title": translated_attribute("Title"),
-      "subtitle": has_value?("Subtítulo") ? translated_attribute("Subtítulo") : translated_attribute("Departamento"),
+      "title": localized_attribute("Titulo"),
+      "subtitle": has_value?("Subtítulo") ? localized_attribute("Subtítulo") : localized_attribute("Departamento"),
       "slug": slug_value,
       "hashtag": nil,
-      "short_description": translatable_hash(short_description),
-      "description": translatable_hash(description_html),
+      "short_description": localized(short_description),
+      "description": localized(description_html),
       "announcement": nil,
       "start_date": start_date,
       "end_date": end_date,
-      "remote_hero_image_url": nil,
-      "remote_banner_image_url": nil,
-      "developer_group": translated_attribute("Unidad responsable"),
+      "remote_hero_image_url": image_url,
+      "remote_banner_image_url": image_url,
+      "developer_group": localized_attribute("Unidad responsable"),
       "local_area": nil,
       "meta_scope": nil,
       "participatory_scope": nil,
@@ -48,7 +60,10 @@ class ProcessesParser
       "scope": scope_data,
       "attachments": { "files": nil },
       "components": nil,
-      "scopes_enabled": true
+      "scopes_enabled": true,
+      "external_es_id": external_es_id,
+      "original_id": { locale => external_id },
+      "locale": locale
     }
   end
 
@@ -56,11 +71,14 @@ class ProcessesParser
     start_date_string = start_date.to_date.to_s
     end_date_string = end_date&.to_date&.to_s
     {
-      original_id: raw_content["NID"],
+      original_id: external_id,
       description_paragraphs_count: splitted_description.count,
-      raw_description: raw_content["Descripción HTML"],
+      raw_description: raw_content["Descripcion - HTML"],
       first_paragraph_description: short_description,
       original_url: "https://gobiernoabierto.navarra.es#{raw_content["Ruta"]}",
+      locale: locale,
+      original_language: raw_content["Idioma"],
+      es_node_id: raw_content["Nodo castellano"],
       proposal_status: proposal_status,
       participation_status: participation_status,
       start_and_end_date_coincident: start_date_string == end_date_string,
@@ -73,20 +91,32 @@ class ProcessesParser
 
   private
 
+  def image_url
+    @image_url ||= raw_content["Imagen URL"].presence&.gsub(/\s/, "")
+  end
+
+  def external_es_id
+    @external_es_id ||= raw_content["Nodo castellano"].strip.presence
+  end
+
+  def external_id
+    @external_id ||= raw_content["NID"].strip.presence
+  end
+
   def short_description
     splitted_description.first.to_s
   end
 
   def remaining_description
-    raw_content["Descripción HTML"]
+    raw_content["Descripcion - HTML"]
   end
 
   def splitted_description
-    @splitted_description ||= Nokogiri::HTML.parse(raw_content["Descripción HTML"]).text.split("\n").select(&:present?)
+    @splitted_description ||= Nokogiri::HTML.parse(raw_content["Descripcion - HTML"]).text.split("\n").select(&:present?)
   end
 
   def split_description_by_tags(*tags)
-    html = raw_content["Descripción HTML"]
+    html = raw_content["Descripcion - HTML"]
     tags.each do |tag|
       next if (elements = Nokogiri::HTML.parse(html).css(tag)).blank?
 
@@ -111,17 +141,33 @@ class ProcessesParser
 
     return value if value.present?
 
-    slug(raw_content["Title"])
+    slug(raw_content["Titulo"])
   end
 
   def documentation
-    return unless has_value?("Documentacion")
+    return unless has_value?("Documentacion URL")
+
+    urls = raw_content["Documentacion URL"].split(",").map(&:strip)
+
+    descriptions = if urls.count > 1
+                     raw_content["Documentacion"].split(",").map(&:strip)
+                   else
+                     [raw_content["Documentacion"].presence&.strip]
+                   end.compact
+
+    descriptions = urls if descriptions.count != urls.count
+
+    links_list = descriptions.zip(urls).map do |description, url|
+      "<li><a href=\"#{url}\">#{description}</a></li>"
+    end.join("\n")
 
     <<-HTML
     <div>
       <p>
-        <h3>Documentación</h3>
-         #{raw_content["Documentacion"]}
+        <h3>#{translate(:documentation)}</h3>
+        <ul>
+           #{links_list}
+        </ul>
       </p>
     </div>
     HTML
@@ -144,7 +190,7 @@ class ProcessesParser
     <<-HTML
     <div>
       <p>
-        <h3>Enlaces</h3>
+        <h3>#{translate(:links)}</h3>
         <ul>
          #{links_list}
         </ul>
@@ -154,7 +200,15 @@ class ProcessesParser
   end
 
   def title
-    translated_attribute("Title")
+    localized_attribute("Titulo")
+  end
+
+  def translate(key)
+    TRANSLATIONS[locale][key]
+  end
+
+  def localized(text)
+    { locale => text }
   end
 
   def start_date
@@ -195,8 +249,8 @@ class ProcessesParser
 
   def area_type
     @area_type ||= organization.area_types.find_or_create_by(
-      name: translatable_hash("Tipo de propuesta"),
-      plural: translatable_hash("Tipos de propuesta")
+      name: { es: "Tipo de propuesta", eu: "Proposamen mota" },
+      plural: { es: "Tipos de propuesta", eu: "Proposamen motak" }
     )
   end
 
@@ -224,20 +278,20 @@ class ProcessesParser
 
   def legislature_scope_type
     @legislature_scope_type ||= organization.scope_types.find_or_create_by(
-      name: translatable_hash("Legislatura"),
-      plural: translatable_hash("Legislaturas")
+      name: { es: "Legislatura", eu: "Legebiltzarra" },
+      plural: { es: "Legislaturas", eu: "Legebiltzarrak" }
     )
   end
 
   def department_scope_type
     @department_scope_type ||= organization.scope_types.find_or_create_by(
-      name: translatable_hash("Departamento"),
-      plural: translatable_hash("Departamentos")
+      name: { es: "Departamento", eu: "Sail" },
+      plural: { es: "Departamentos", eu: "Sailak" }
     )
   end
 
-  def translated_attribute(attribute)
-    translatable_hash(raw_content[attribute])
+  def localized_attribute(attribute)
+    localized(raw_content[attribute])
   end
 
   def translatable_hash(text)
