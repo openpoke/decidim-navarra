b8d987ce0277fc60cda2ad2a9e6d9d804d65b0d4
d040110 <eduardo@populate.tools>
2021-06-02 13:46:56 +0200

    Use https by default in action_mailer in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,6 +64,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "decidim_navarra_#{Rails.env}"
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.default_url_options = { protocol: "https" }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
