903ea5910c1a08b9a0e28c9b380ab17c64f63b81
d040110 <eduardo@populate.tools>
2021-10-15 11:47:17 +0200

    Update migrations

--- db/migrate/20210531162997_move_signature_type_to_initative_type.decidim_initiatives.rb
+++ db/migrate/20210531162997_move_signature_type_to_initative_type.decidim_initiatives.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 # This migration comes from decidim_initiatives (originally 20191002082220)
 
+# Migration updated after 0.25 upgrade
 class MoveSignatureTypeToInitativeType < ActiveRecord::Migration[5.2]
   class InitiativesType < ApplicationRecord
     self.table_name = :decidim_initiatives_types
@@ -23,7 +24,7 @@ class MoveSignatureTypeToInitativeType < ActiveRecord::Migration[5.2]
 
     InitiativesType.reset_column_information
 
-    Decidim::Initiatives::InitiativesType.find_each do |type|
+    InitiativesType.find_each do |type|
       type.signature_type = if type.online_signature_enabled && face_to_face_voting_allowed
                               :any
                             elsif type.online_signature_enabled && !face_to_face_voting_allowed
