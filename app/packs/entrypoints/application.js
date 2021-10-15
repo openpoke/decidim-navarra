84fdb8a7da0e34f7f2d01eba9f14273e292fa1df
d040110 <eduardo@populate.tools>
2021-10-15 13:39:57 +0200

    Replace image_tag with image_pack_tag

--- app/packs/entrypoints/application.js
+++ app/packs/entrypoints/application.js
@@ -10,9 +10,9 @@
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
-//
-// const images = require.context('../images', true)
-// const imagePath = (name) => images(name, true)
+
+ const images = require.context('../images', true)
+ const imagePath = (name) => images(name, true)
 
 // Activate Active Storage
 // import * as ActiveStorage from "@rails/activestorage"
