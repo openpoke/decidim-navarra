9c82cbdbe6383f4243801da78c5d3b1cae7755f7
d040110 <eduardo@populate.tools>
2024-04-09 20:05:06 +0200

    Add footer contact depend conditionally on configuration and organization

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def footer_contact
+    @footer_contact ||= Rails.application.secrets[:footer_contact].to_h.with_indifferent_access[current_organization&.name]
+  end
 end
