558e088bc5362cec9354b3df31e0a54fae78abab
d040110 <eduardo@populate.tools>
2024-04-15 12:07:48 +0200

    Fix footer_contact method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
   def footer_contact
-    @footer_contact ||= Rails.application.secrets[:footer_contact].to_h.with_indifferent_access[current_organization&.name]
+    @footer_contact ||= JSON.parse(Rails.application.secrets[:footer_contact].presence || "{}").with_indifferent_access[current_organization&.name]
   end
 end
