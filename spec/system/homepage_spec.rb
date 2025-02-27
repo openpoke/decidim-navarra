afecbb77fb34a68b2706ea1d0dd51db72be07290
elviabth <ejbenedith@gmail.com>
2025-02-27 12:23:34 +0100

    add specs

---
+++ spec/system/homepage_spec.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+describe "Homepage", perform_enqueued: true do
+  let(:organization) { create(:organization) }
+  let!(:content_block) { create(:content_block, organization:, manifest_name: :hero) }
+
+  before do
+    switch_to_host(organization.host)
+  end
+
+  it "renders the home page" do
+    visit decidim.root_path
+    expect(page).to have_content("Welcome")
+  end
+end
