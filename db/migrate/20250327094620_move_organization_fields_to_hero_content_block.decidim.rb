f2a6e51b6c07e48c3a4be3a717616c1cb78341d0
d954903 <ivan@pokecode.net>
2025-04-04 09:41:01 +0200

    fix migration

--- db/migrate/20250327094620_move_organization_fields_to_hero_content_block.decidim.rb
+++ db/migrate/20250327094620_move_organization_fields_to_hero_content_block.decidim.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 # This migration comes from decidim (originally 20180810092428)
 
 class MoveOrganizationFieldsToHeroContentBlock < ActiveRecord::Migration[5.2]
@@ -12,17 +13,17 @@ class MoveOrganizationFieldsToHeroContentBlock < ActiveRecord::Migration[5.2]
   end
 
   def change
-    Decidim::ContentBlock.reset_column_information
-    Organization.find_each do |organization|
-      content_block = Decidim::ContentBlock.find_by(organization: organization, scope: :homepage, manifest_name: :hero)
-      settings = {}
-      welcome_text = organization.welcome_text || {}
-      settings = welcome_text.inject(settings) { |acc, (k, v)| acc.update("welcome_text_#{k}" => v) }
+    # Decidim::ContentBlock.reset_column_information
+    # Organization.find_each do |organization|
+    #   content_block = Decidim::ContentBlock.find_by(organization: organization, scope: :homepage, manifest_name: :hero)
+    #   settings = {}
+    #   welcome_text = organization.welcome_text || {}
+    #   settings = welcome_text.inject(settings) { |acc, (k, v)| acc.update("welcome_text_#{k}" => v) }
 
-      content_block.settings = settings
-      content_block.settings_will_change!
-      content_block.save!
-    end
+    #   content_block.settings = settings
+    #   content_block.settings_will_change!
+    #   content_block.save!
+    # end
 
     remove_column :decidim_organizations, :welcome_text
     remove_column :decidim_organizations, :homepage_image
