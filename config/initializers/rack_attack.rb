bf0026a4f7838c355c010a059ad9f49dbc5e2566
David <david@pokecode.net>
2025-10-30 15:04:28 +0100

    Bypass allowed ips (#5)
    * allow rack-attack to bypass allowed ips
    
    * ca
    
    ---------
    
    Co-authored-by: Ivan Vergés <ivan@pokecode.net>

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -1,9 +1,6 @@
 # frozen_string_literal: true
 
 if Rails.env.production?
-  # TODO: add internal canodrom ip here
-  # Rack::Attack.safelist_ip("5.6.7.0/24")
-
   # Provided that trusted users use an HTTP request param named skip_rack_attack
   # with this you can perform apache benchmark test like this:
   # ab -n 2000 -c 20 'https://decidim.url/?skip_rack_attack=some-secret'
@@ -20,5 +17,12 @@ if Rails.env.production?
   Rack::Attack.safelist("bypass with secret param") do |request|
     # Requests are allowed if the return value is truthy
     request.params["skip_rack_attack"] == Rails.application.secrets.rack_attack_skip
+  end
+
+  allowed_ips = ENV.fetch("RACK_ATTACK_ALLOWED_IPS", nil)
+  if allowed_ips.present?
+    allowed_ips.split(",").each do |ip|
+      Rack::Attack.safelist_ip(ip)
+    end
   end
 end
