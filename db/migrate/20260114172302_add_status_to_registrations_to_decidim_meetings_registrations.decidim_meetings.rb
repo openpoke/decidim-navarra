3394f19da1481cc61310eaa171b723b4e981d2e7
Ivan Vergés <ivan@pokecode.net>
2026-01-14 21:24:25 +0100

    fix migrations

--- db/migrate/20260114172302_add_status_to_registrations_to_decidim_meetings_registrations.decidim_meetings.rb
+++ db/migrate/20260114172302_add_status_to_registrations_to_decidim_meetings_registrations.decidim_meetings.rb
@@ -3,7 +3,7 @@
 # This migration comes from decidim_meetings (originally 20250408071941)
 class AddStatusToRegistrationsToDecidimMeetingsRegistrations < ActiveRecord::Migration[7.0]
   def change
-    add_column :decidim_meetings_registrations, :status, :string, default: "registered"
-    add_index :decidim_meetings_registrations, :status
+    add_column :decidim_meetings_registrations, :status, :string, default: "registered" unless column_exists?(:decidim_meetings_registrations, :status)
+    add_index :decidim_meetings_registrations, :status unless index_exists?(:decidim_meetings_registrations, :status)
   end
 end
